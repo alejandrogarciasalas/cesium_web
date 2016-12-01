574ecf76d26cdc6f5ebd186b896e34248799055c
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-12-01 14:02:38 -0800

    add bokeh, python side

--- cesium_app/plot.py
+++ cesium_app/plot.py
@@ -1,11 +1,9 @@
+from itertools import cycle, islice
 import numpy as np
-import pandas as pd
-from sklearn.metrics import confusion_matrix
-import xarray as xr
-import plotly
-import plotly.offline as py
-from plotly.tools import FigureFactory as FF
-
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.layouts import gridplot
+from bokeh.palettes import PuBu as palette
 from cesium import featureset
 from .config import cfg
 
@@ -22,27 +20,51 @@ def feature_scatterplot(fset_path, features_to_plot):
 
     Returns
     -------
-    (fig.data, fig.layout)
-        Returns (fig.data, fig.layout) where `fig` is an instance of
-        `plotly.tools.FigureFactory`.
+    json
+        Returns d.to_json where `d` is an instance of
+        `bokeh.Document`.
     """
-    with featureset.from_netcdf(fset_path, engine=cfg['xr_engine']) as fset:
-        feat_df = fset.to_dataframe()
-        feat_df = feat_df[features_to_plot]
 
-        if 'target' in fset:
-            feat_df['target'] = fset.target.values
-            index = 'target'
-        else:
-            index = None
+    fset = featureset.from_netcdf(fset_path, engine=cfg['xr_engine'])
+    fset = fset.drop('target')
+    X = fset.to_dataframe()[features_to_plot]
+    if 'target' in fset and fset.target.values.dtype != np.float:
+        y = fset.target.values
+        labels = np.unique(y)
+    else:
+        y = [None] * len(X)
+        labels = [None]
 
-    # TODO replace 'trace {i}' with class labels
-    fig = FF.create_scatterplotmatrix(feat_df, diag='box', index=index,
-                                      height=800, width=800)
+    if len(labels) in palette:
+        colors = palette[len(labels)]
+    else:
+        all_colors = sorted(palette.items(), key=lambda x: x[0],
+                            reverse=True)[0][1]
+        colors = list(islice(cycle(all_colors), len(labels)))
 
-    py.plot(fig, auto_open=False, output_type='div')
+    plots = np.array([[figure(width=300, height=200)
+                       for j in range(len(features_to_plot))]
+                      for i in range(len(features_to_plot))])
+    for (i, j), p in np.ndenumerate(plots):
+        for l, c in zip(labels, colors):
+            if l is not None:
+                inds = np.where(y == l)[0]
+            else:
+                inds = np.arange(len(X))
+            p.circle(X.values[inds, i], X.values[inds, j], color=c,
+                     legend=(l if (i == j and l is not None) else None))
+            p.legend.location = 'bottom_right'
+            p.legend.label_text_font_size = '6pt'
+            p.legend.spacing = 0
+            p.legend.padding = 0
+            p.xaxis.axis_label = features_to_plot[i]
+            p.yaxis.axis_label = features_to_plot[j]
 
-    return fig.data, fig.layout
+    plot = gridplot(plots.tolist(), ncol=len(features_to_plot), mergetools=True)
+    d = Document()
+    d.add_root(p)
+    json = d.to_json()
+    return json
 
 
 #def prediction_heatmap(pred_path):
